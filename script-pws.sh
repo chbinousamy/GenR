28ed9472e455375487b01931d4cd96f0b9a0b563
Samy CHBINOU <samy.chbinou@esna.cloud>
2024-10-31 22:52:04 +0100

    Update script-pws.sh

--- script-pws.sh
+++ script-pws.sh
@@ -67,6 +67,6 @@ rm data
 curl_data="$curl_data}]}" && echo $curl_data | jq > latest.json && cat latest.json && echo
 curl -X POST -H "Content-Type: application/json" --data @latest.json https://stations.windy.com/pws/update/${WINDY_API_KEY}
 echo 
-./update_data.js
+node update_data.js
 date "+%H:%M:%S %d/%m/%y"
 
